261256e485b9f04caedaaa016e30950a679160bd
Dustin Hammack <rdhammack88@hotmail.com>
2018-01-29 05:01:54 -0600

    Add files via upload

--- includes/footer.php
+++ includes/footer.php
@@ -2,5 +2,5 @@
 	<p>&copy;2017 - <a href="http://www.dustinhammack.com" target="_blank">Dustin Hammack</a></p>
 </footer>
 
-<script src="js/main.js"></script>
+<script src="<?php echo $doc_root; ?>js/main.js"></script>
 </body>
